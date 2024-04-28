fea29efeacbc777374c87a0621df40dca80d994c
MelinaZakeriM9 <www.melinazakerim9@gmail.com>
2024-04-28 20:31:45 +0330

    Update LiarLiar.py

--- LiarLiar.py
+++ LiarLiar.py
@@ -3,8 +3,22 @@ import random
 
 # a3 is necessarilly true
 # a2, d1 and d3 determine the convict 
-# 1s are True and -1s are False
 
+def only_one(a2, d1, d3):
+    """checks how many of the arguements are "true"
+    Note: only one of the arguements can be logically true
+    """
+    Ts = 0
+    for i in [a2, d1, d3]:
+        if i == 1:
+            Ts +=1
+
+    if Ts <= 1:
+        return True
+    else:
+        return False
+
+# 1s are True and -1s are False
 def logic(a2, d1, d3):
     b3 = -a2
     c2 = -a2
@@ -17,35 +31,33 @@ def logic(a2, d1, d3):
     a1 = a2
     c1 = -a1
 
-    Ws= 0
-    Ls= 0
+    Ts = 0
+    Fs = 0
     for i in [a1, a2, b1, b2, b3, c1, c2, c3, d1, d2, d3]:
         if i == 1:
-            Ws += 1
+            Ts += 1
         elif i == -1:
-            Ls += 1
+            Fs += 1
 
-    
-    if Ws == 5 and Ls == 6:
+    if Ts == 5 and Fs == 6:
         return True
-
-
 
 while True:
     a2 = random.choice([-1, 1])
     d1 = random.choice([-1, 1])
     d3 = random.choice([-1, 1])
     
-    a3 = logic(a2, d1, d3)
+    if only_one(a2, d1, d3):
+        a3 = logic(a2, d1, d3)
 
-    if a3 == True:
-        if a2 == 1:
-            print("B is the thief")
-        elif d1 == 1:
-            print("D is the thief")
-        elif d3 == 1:
-            print("A is the thief")
-        else:
-            print("C is the thief")
+        if a3 == True:
+            if a2 == 1:
+                print("B is the thief")     
+            elif d1 == 1:
+                print("D is the thief")   
+            elif d3 == 1:
+                print("A is the thief")    
+            else:
+                print("C is the thief") 
             
-        break 
+            break
